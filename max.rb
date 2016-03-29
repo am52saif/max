c0a6cadd339192201a9934c53712f5370ca56f60
am52saif <aliasgar.s.merchant@gmail.com>
2016-03-29 18:12:04 +0000

    Adding Max

---
+++ max.rb
@@ -0,0 +1,27 @@
+def maximum(arr)
+	num = 0
+	dummy = arr[num]
+	while num < arr.length
+		if dummy <= arr[num]
+			dummy = arr[num]
+		end
+		num +=1
+	end
+	dummy
+end
+
+
+
+# expect it to return 42 below
+result = maximum([2, 42, 22, 02])
+puts "max of 2, 42, 22, 02 is: #{result}"
+
+# expect it to return nil when empty array is passed in
+result = maximum([])
+puts "max on empty set is: #{result.inspect}"
+
+result = maximum([-23, 0, -3])
+puts "max of -23, 0, -3 is: #{result}"
+
+result = maximum([6])
+puts "max of just 6 is: #{result}"
